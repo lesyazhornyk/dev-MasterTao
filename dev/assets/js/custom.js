6424daac54b02eb8be94819f1a1b6d0a6006dc96
Lesya <lesyazhornyk50@gmail.com>
2023-03-09 23:15:51 +0100

    Add footer

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
     $('.banner').slick({
+    //dots: true,
+    arrow: true,
+    appendArrows: ".banner-nav",
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+           arrows: false,
+           dots: true,
+          }
+        },
 
+      ]
+   
     });
+    
 });
