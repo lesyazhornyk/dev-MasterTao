7756f6cfa998f722c8ddf43abf854b7e1b7aa8a3
Lesya <lesyazhornyk50@gmail.com>
2023-04-10 15:05:43 -0600

    Add calculation block

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,3 +1,25 @@
+const SECTION = document.guerySelectorAll('section');
+const CONTAINER_FLUID = document.guerySelectorAll('container-fluid');
+console.log('CONTAINER_FLUID');
+
+
+CONTAINER_FLUID.forEach(element => {
+const sectionHasFluid = element.closest('section')
+
+if (sectionHasFluid.classList.contains('first-screen')){
+null
+} else {
+sectionHasFluid.closest('section').style.padding = '0 50px'
+}
+})
+
+
+
+
+
+
+
+
 $( document ).ready(function() {
     $('.banner').slick({
     //dots: true,
@@ -17,3 +39,17 @@ $( document ).ready(function() {
     });
     
 });
+
+
+
+
+const btnBox = document.querySelectorAll('.btn-box');
+
+btnBox.forEach(item => {
+
+item.closest('.col-md-6').classList.add('button-group');
+})
+document.retElementById('fileInput').onchange = function(){
+document .getElementById('file-name').innecHTML = this.files[0].name;
+};
+
