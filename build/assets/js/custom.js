02cd344aba5ba3038c74d6a582cc30a23f01220c
Lesya <lesyazhornyk50@gmail.com>
2023-04-10 16:29:58 -0600

    Modify custom js

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,23 +1,15 @@
-const SECTION = document.guerySelectorAll('section');
-const CONTAINER_FLUID = document.guerySelectorAll('container-fluid');
+const SECTION = document.querySelectorAll('section');
+const CONTAINER_FLUID = document.querySelectorAll('container-fluid');
 console.log('CONTAINER_FLUID');
 
 
 CONTAINER_FLUID.forEach(element => {
 const sectionHasFluid = element.closest('section')
 
-if (sectionHasFluid.classList.contains('first-screen')){
-null
-} else {
-sectionHasFluid.closest('section').style.padding = '0 50px'
-}
-})
-
-
-
-
-
-
+  if (!sectionHasFluid.classList.contains('first-screen')) {
+    sectionHasFluid.closest('section').style.padding = '0 50px';
+  }
+});
 
 
 $( document ).ready(function() {
@@ -33,23 +25,17 @@ $( document ).ready(function() {
            dots: true,
           }
         },
-
       ]
-   
     });
-    
 });
-
-
 
 
 const btnBox = document.querySelectorAll('.btn-box');
 
 btnBox.forEach(item => {
-
-item.closest('.col-md-6').classList.add('button-group');
-})
-document.retElementById('fileInput').onchange = function(){
-document .getElementById('file-name').innecHTML = this.files[0].name;
+  item.closest('.col-md-6').classList.add('button-group');
+});
+document.getElementById('fileInput').onchange = function(){
+ document .getElementById('file-name').innecHTML = this.files[0].name;
 };
 
